3a19661471f36788189d43525be624c2a5cdea8b
Oleksandr-frontend <129508922+Oleksandr-frontend@users.noreply.github.com>
2023-04-26 14:01:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
-    $(".burger").click(function () { 
-        $(".burger").toggleClass("active")
+    $(".burger").click(function(){
+        $(".burger").toggleClass("active");
         $(".nav").toggleClass("active")
         $("body").toggleClass("hiden")
-     })
+        $(".btn-up").toggleClass("clouse")
+    })
+
+    // buton up
+    $(window).scroll(function(){
+        if($(this).scrollTop() >= 200){
+            $(".btn-up").fadeIn()
+        } else{
+            $(".btn-up").fadeOut()
+        }
+    })
+
+    $(".btn-up").click(function(){
+        $("body, html").animate({
+            scrollTop: 0
+        }, 800)
+    })
+
+    // scroll animate
+    $("a[href^='#']").click(function(e){
+        e.preventDefault();
+        let element = $(this).attr("href")
+        $("body,html").animate({
+            scrollTop: $(element).offset().top
+        }, 2000)
+    })
 });
